6934da0ca68fe989212388b196ff088608d93010
Rishabh Ojha <rishabhojha11@gmail.com>
2022-07-04 16:56:47 +0530

    Removed unnecessary files and added more configurations in settings

--- djangoprod/config/settings/__init__.py
+++ djangoprod/config/settings/__init__.py
@@ -4,7 +4,9 @@ Init doctor for initializing settings
 """
 from .setup import ENV
 
-if ENV("ENVIRONMENT")=="STAGE1":
-    pass
+if ENV("ENVIRONMENT")=="PROD":
+    from .core_prod import *
+elif ENV("ENVIRONMENT")=="UAT":
+    from .core_uat import *
 else:
     from .core_dev import *
